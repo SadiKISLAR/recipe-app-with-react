33fc44a5568b0ec26f852fc75a2ca3399d93beb7
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-17 23:18:35 +0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import './App.css';
 import AppRouter from './router/AppRouter';
+import theme from "./components/globalStyles/theme";
 
 function App() {
   return (
-    <ThemeProvider >
+    <ThemeProvider theme={theme}>
       <AppRouter />
     </ThemeProvider>
   );
