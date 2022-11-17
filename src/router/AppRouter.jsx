2d83c8e2ca5c69c259d1a56ecbc5f643a71a0593
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-17 22:01:22 +0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { GlobalStyles } from '../components/globalStyles/GlobalStyles'
+import Navbar from '../components/nav/Navbar'
+import About from '../pages/about/About';
+import Detail from '../pages/detail/Detail';
+import Home from '../pages/home/Home';
+import Login from '../pages/login/Login';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='about' element={<About />} />
+        <Route path='detail' element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter
---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PrivateRouter = () => {
+
+  const user = true;
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default PrivateRouter
