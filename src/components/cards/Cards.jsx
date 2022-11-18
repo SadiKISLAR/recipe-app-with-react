9a37b16a92dd37f633780add489a5ba322a70659
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-18 15:22:10 +0300

    card done

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Card, Header, MainContainer } from './Cards.style'
+import { Button, Card, Header, Image, MainContainer } from './Cards.style'
+import defaultImage from "../../assets/default-image.jpg"
+import { useNavigate } from 'react-router-dom'
+
+
 
 const Cards = ({ recipes }) => {
+  const navigate = useNavigate();
   return (
     <MainContainer wrap="wrap">
       {recipes.map(({ recipe }, index) => {
         return (
           <Card key={index}>
             <Header> {recipe.label}</Header>
+            <Image src={recipe.image || defaultImage} />
+            <Button
+              onClick={() => navigate("detail", { state: recipe, replace: false })}>View More</Button>
           </Card>
         )
       })}
